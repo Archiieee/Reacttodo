5186792478a7251c679049196b089342ee22b052
Archiieee <archishman02.c@gmail.com>
2024-02-09 13:42:46 +0530

    update

--- src/components/ToDolist.jsx
+++ src/components/ToDolist.jsx
@@ -16,6 +16,8 @@ const ToDoList = ({ fetchTasks }) => {
     fetchCategories();
   }, [fetchTasks]);
 
+
+  
   const fetchCategories = async () => {
     try {
       const response = await axios.get('http://localhost:5000/categories');
